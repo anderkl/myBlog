0f697d8e3b9914462763e7e6031f688df0dc4a75
吴鹏飞 <1512964764@qq.com>
2021-11-09 20:07:43 +0800

    完成了更新

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -140,62 +140,62 @@ module.exports = {
                 {
                     name: '大鱼-钢琴版',
                     artist: '赵海洋',
-                    url: 'https://webfs.ali.kugou.com/202111080113/67904648a3d653afe247657218b202e1/G123/M02/01/1B/W5QEAFyNzJKAGgWWAEgoAhcaQs4810.mp3',
+                    url: ' https://m701.music.126.net/20211109201640/b177e9d6a17995399d6f31a1393f9dad/jdyyaac/0e09/055d/5608/9c69f6b3d5278ce15da8abc17f785f70.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20200114/20200114102808361228.jpg'
                 },
                 {
                     name: '菊次郎的夏天钢琴曲',
                     artist: '赵超',
-                    url: 'https://webfs.ali.kugou.com/202111080135/70de9bc6e6b853629ebb00dbdc1ff2c9/G168/M03/0A/09/iJQEAF0vA4KAYkk7ACQl4S4xNlo754.mp3',
+                    url: 'https://m701.music.126.net/20211109201956/188f611272b20ec3bfcc59666a5b4ada/jdyyaac/515e/555f/010c/f4f24cf39cb5eee1e60d6feebc237c12.m4a',
                     cover: 'https://imgessl.kugou.com/uploadpic/softhead/400/20190322/20190322013103293393.jpg'
                 },
                 {
                     name: '天空之城',
                     artist: '五个半柠檬',
-                    url: 'https://webfs.ali.kugou.com/202111080109/f9d6aeb2d779cad57846a06ebe9717bb/KGTX/CLTX001/df68d572d3b4c1b91f75ab05ced821cb.mp3',
+                    url: 'https://m10.music.126.net/20211109202108/bd203a6e01d6f43297f60b94a5668389/yyaac/obj/wonDkMOGw6XDiTHCmMOi/2395098316/588d/abb0/2e24/e1b411648844d590e299267da4c49bd6.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20211015/20211015192801721862.jpg'
                 },
 
                 {
                     name: '卡农-钢琴版',
                     artist: 'dylanf',
-                    url: 'https://webfs.ali.kugou.com/202111080116/7530b4e3dde0ca53575a832e8cf2ff6b/KGTX/CLTX001/9145a28bda0cb8a49c7868890ecbcc53.mp3',
+                    url: 'https://m10.music.126.net/20211109202146/6264872d5282b388acb4546ed14541fc/yyaac/0109/045e/0f59/6c34192a3e3463a31bd88326790f2d3b.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20211102/20211102004916157050.jpg'
                 },
                 {
                     name: '土耳其进行曲',
                     artist: 'jaycd',
-                    url: 'https://webfs.ali.kugou.com/202111080119/e7f8c93f89f0f6451830f701f0e7ba6b/KGTX/CLTX001/1f469b770f3823d066e1fb024d8241de.mp3',
+                    url: 'https://m701.music.126.net/20211109202223/ce05fe504fbd1de88daf39823e2fac73/jdyyaac/545c/565a/0f5e/c6cab9fc84a03f450ab7842b6f905697.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20201221/20201221152502420575.jpg'
                 },
                 {
                     name: '水边的阿狄丽娜',
                     artist: '钢琴先森',
-                    url: 'https://webfs.ali.kugou.com/202111080123/f02ca3351a62a206f580fc2f6d390a85/G204/M03/1F/07/DA4DAF5pIlCAPTzuACaiyAW6u28263.mp3',
+                    url: 'https://m701.music.126.net/20211109202258/4f9ada581a936f96a979a413322daed9/jdyyaac/020f/0609/020b/5e6a84d7191c5f6a28845a388f75c32e.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20200416/20200416060347489322.jpg'
                 },
                 {
                     name: '梦中的婚礼',
                     artist: 'Richard Clayderman',
-                    url: 'https://webfs.ali.kugou.com/202111080124/e4f72bbb5ed4e1538096d905f3f50104/G212/M0B/13/17/FA4DAF6zLHiAW4O_ACiUJniLB3Y814.mp3',
+                    url: 'https://m701.music.126.net/20211109202336/45acb22ef565bec5a5baa0b5b87a027f/jdyyaac/555c/035c/045a/b6261cbcb646a2da4d733004fd5c0c0b.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20200421/20200421074910356505.jpg'
                 },
                 {
                     name: '匆匆那年',
                     artist: 'LIU 9',
-                    url: 'https://webfs.ali.kugou.com/202111080129/7bf020602a659698a686935c05d7adfa/G234/M02/14/09/ypQEAF9ptLKAOqXcACWzeBpUlUQ992.mp3',
+                    url: 'https://m801.music.126.net/20211109202414/4b63f79f8c86d50b62fb8389274b2e06/jdyyaac/0e5b/0208/015d/911e14c4525bfd0128d56737b59e8db1.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20160907/20160907221313754659.jpg'
                 },
                 {
                     name: '所念皆星河 钢琴',
                     artist: '纯音乐',
-                    url: 'https://webfs.ali.kugou.com/202111080152/ab392148cfd2e9968c79ce4446384c38/KGTX/CLTX001/c8d4734a16b39bafc0cb5c16e46dcf42.mp3',
+                    url: 'https://m10.music.126.net/20211109202447/90ae22b044e1b75d3bfa5419c35c3c11/yyaac/obj/wonDkMOGw6XDiTHCmMOi/1649510747/f05b/0103/fefd/dd1e96a9e6b69bb2f8d7ae5a6b13a932.m4a',
                     cover: 'https://imgessl.kugou.com/stdmusic/20211015/20211015192502772874.jpg'
                 },
                 {
                     name: '克罗地亚狂想曲钢琴',
                     artist: '纯音乐',
-                    url: 'https://webfs.ali.kugou.com/202111080132/ca188ee5ad0539b317ec159def822262/G192/M09/08/14/AA4DAF5W1YmAH9_LADfp97dqdoY747.mp3',
+                    url: 'https://m10.music.126.net/20211109202527/89283af653dcc301fbf4508e65234572/yyaac/0009/055e/0409/7c2ab3ec0f24a8bbf1943ccc71de5efa.m4a',
                     cover: 'https://imgessl.kugou.com/uploadpic/softhead/400/20130531/20130531162457903.jpg'
                 }
             ],
